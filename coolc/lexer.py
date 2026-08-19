8b6ef1736fd3b55c693d7277494ff8b2ecd9bd39
Edgar Lutterbach <edgar.lutterbach@gmail.com>
2026-08-19 06:44:23 -0300

    upt: atualizando lexer com função para pular espaços em branco e comentários

--- coolc/lexer.py
+++ coolc/lexer.py
@@ -1,10 +1,23 @@
+import string
+
 from coolc.tokens import TokenType, Token, KEYWORDS
+
+LETTERS = set(string.ascii_letters)
+DIGITS = set(string.digits)
+WHITESPACES = set(string.whitespace)
+
+class LexicalError(Exception):
+    def __init__(self, message, line):
+        super().__init__(message)
+        self.message = message
+        self.line = line
 
 class Lexer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
         self.line = 1
+        self.token_line = 1
 
     def at_end(self):
         return self.pos >= len(self.text)
@@ -27,3 +40,73 @@ class Lexer:
             return False
         self.advance()
         return True
+
+    def next_token(self):
+        self.skip_whitespaces_and_comments()
+        self.token_line = self.line
+
+        if self.at_end():
+            return self.make_token(TokenType.EOF)
+
+        char = self.peek()
+
+        if char in LETTERS or char == "_":
+            return self.read_identifier()
+        if char in DIGITS:
+            return self.read_number()
+        if char == '"':
+            return self.read_string()
+        return self.read_operator()
+
+    def make_token(self, token_type, value=None):
+        return Token(token_type, self.token_line, value)
+
+    def skip_whitespaces_and_comments(self):
+        while True:
+            if self.peek() in WHITESPACES:
+                self.advance()
+
+                continue
+
+            if self.peek() == "-" and self.peek(1) == "-":
+                self.advance()
+                self.advance()
+
+                while not self.at_end() and self.peek() != "\n":
+                    self.advance()
+                continue
+
+            if self.peek() == "(" and self.peek(1) == "*":
+                cont = 1
+                start_line = self.line
+                self.advance()
+                self.advance()
+
+                while cont != 0 and not self.at_end():
+                    if self.peek() == "(" and self.peek(1) == "*":
+                        cont += 1
+                        self.advance()
+                        self.advance()
+                    elif self.peek() == "*" and self.peek(1) == ")":
+                        cont -= 1
+                        self.advance()
+                        self.advance()
+                    else:
+                        self.advance()
+
+                if cont != 0:
+                    raise LexicalError("Comentário de bloco não foi fechado", start_line)
+                continue
+            break
+
+    def read_identifier(self):
+        raise NotImplementedError
+
+    def read_number(self):
+        raise NotImplementedError
+
+    def read_string(self):
+        raise NotImplementedError
+
+    def read_operator(self):
+        raise NotImplementedError
